9adda0189e8cf96e7bd09a4c49a17b6df9f4342e
haphut <haphut@gmail.com>
2015-04-02 16:39:33 +0300

    Fix custom lodash building.

--- build_lodash.sh
+++ build_lodash.sh
@@ -2,32 +2,49 @@
 #
 # Build a custom lodash to reduce size.
 
-OUTPUT='lodash_build/lodash.min.js'
+PACKAGE_VERSION="`npm list --depth=0 lodash-cli | grep lodash-cli | sed 's/.*@\([[:digit:]][[:digit:]]*\.[[:digit:]][[:digit:]]*\.[[:digit:]][[:digit:]]*\).*$/\1/'`"
 LODASH='node_modules/.bin/lodash'
+BUILD_DIR='./lodash_custom'
+PACKAGE_JSON="${BUILD_DIR}/package.json"
+OUTPUT="${BUILD_DIR}/lodash.min.js"
 
-"${LODASH}" -o "${OUTPUT}" -p \
-exports=none \
+sed -i "s/\"version\": \".*\"/\"version\": \"${PACKAGE_VERSION}\"/" "${PACKAGE_JSON}" && \
+"${LODASH}" modern -o "${OUTPUT}" -p \
+exports=node \
 include=\
 at,\
+chain,\
+clone,\
 cloneDeep,\
+constant,\
 every,\
 filter,\
+findLastIndex,\
 first,\
 flatten,\
 forEach,\
+initial,\
 isArray,\
+isBoolean,\
 isDate,\
 isEmpty,\
+isFinite,\
+isNaN,\
 isNumber,\
+isString,\
 last,\
 map,\
 partial,\
+pick,\
 property,\
 range,\
 reject,\
+rest,\
 size,\
 slice,\
+some,\
+sortedIndex,\
+spread,\
 takeWhile,\
+times,\
 zip
-
-# FIXME: Automatically sed the version in package.json from lodash-cli.
