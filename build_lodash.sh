72c4165871533443e92e3697acba06dec917dfb3
haphut <haphut@gmail.com>
2015-04-23 12:18:44 +0300

    Update lodash building script.

--- build_lodash.sh
+++ build_lodash.sh
@@ -3,12 +3,21 @@
 # Build a custom lodash to reduce size.
 
 PACKAGE_VERSION="`npm list --depth=0 lodash-cli | grep lodash-cli | sed 's/.*@\([[:digit:]][[:digit:]]*\.[[:digit:]][[:digit:]]*\.[[:digit:]][[:digit:]]*\).*$/\1/'`"
-LODASH='node_modules/.bin/lodash'
+LODASH='./node_modules/.bin/lodash'
 BUILD_DIR='./lodash_custom'
+OUTPUT_BASENAME='lodash.js'
 PACKAGE_JSON="${BUILD_DIR}/package.json"
-OUTPUT="${BUILD_DIR}/lodash.min.js"
+OUTPUT="${BUILD_DIR}/${OUTPUT_BASENAME}"
 
-sed -i "s/\"version\": \".*\"/\"version\": \"${PACKAGE_VERSION}\"/" "${PACKAGE_JSON}" && \
+mkdir -p "${BUILD_DIR}" && \
+printf '%s\n' \
+  '{' \
+  '  "name": "lodash",' \
+  "  \"version\": \"${PACKAGE_VERSION}\"," \
+  "  \"main\": \"${OUTPUT_BASENAME}\"," \
+  '  "private": true' \
+  '}' \
+  > "${PACKAGE_JSON}" && \
 "${LODASH}" modern -o "${OUTPUT}" -p \
 exports=node \
 include=\
